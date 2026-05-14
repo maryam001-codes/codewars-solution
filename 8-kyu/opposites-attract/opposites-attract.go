beaf13bee7c893b1430e8cdac6194209b58d7975
Maryam Qozeem <qaasimmaryam15@gmail.com>
2026-05-14 16:59:28 +0100

    Add solution - CodeHub

---
+++ 8-kyu/opposites-attract/opposites-attract.go
@@ -0,0 +1,9 @@
+package kata
+​
+func LoveFunc(flower1, flower2 int) bool {
+  if flower1 % 2 == 0 && flower2 % 2 == 1 || flower1 % 2 == 1 && flower2 % 2 == 0 {
+    return true
+  }
+  return false
+}
+​
