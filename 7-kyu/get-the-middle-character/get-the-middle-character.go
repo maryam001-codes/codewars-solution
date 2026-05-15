abe83d7c4b47166887364138dc8934ffe36e2558
Maryam Qozeem <qaasimmaryam15@gmail.com>
2026-05-15 12:06:22 +0100

    Add solution - CodeHub

---
+++ 7-kyu/get-the-middle-character/get-the-middle-character.go
@@ -0,0 +1,12 @@
+package kata
+​
+func GetMiddle(s string) string {
+  length := len(s)
+  mid := length/2
+  
+  if length % 2 == 0 {
+     return s[mid-1 : mid+1]
+    }
+  return s[mid : mid+1]
+  }
+  
