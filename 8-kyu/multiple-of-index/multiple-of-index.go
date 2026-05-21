9892631d2380e84f652a1df99594660c1702cf7c
Maryam Qozeem <qaasimmaryam15@gmail.com>
2026-05-21 15:14:16 +0100

    Add solution - CodeHub

---
+++ 8-kyu/multiple-of-index/multiple-of-index.go
@@ -0,0 +1,11 @@
+package kata
+​
+func multipleOfIndex (ints []int) []int {
+  var result []int
+  for i := 1; i < len(ints); i++{
+    if ints[i] % i == 0 {
+      result = append(result, ints[i])
+    }
+  }
+  return result
+}
