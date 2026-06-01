636700b4712bb620a51cf33f6a10be7f796c4cf7
Maryam Qozeem <qaasimmaryam15@gmail.com>
2026-06-01 10:07:21 +0100

    Add solution - CodeHub

---
+++ 7-kyu/vowel-count/vowel-count.go
@@ -0,0 +1,12 @@
+package kata
+​
+​
+func GetCount(str string) (count int) {
+  for _, char := range str {
+    switch char {
+      case 'a', 'e', 'i', 'o', 'u':
+      count ++
+    }
+  }
+  return
+}
